608dd0141a5454858151409c2185decbaf5228a4
tricuongrc86 <cuongnht86.work@gmail.com>
2021-09-25 13:51:05 +0700

    TriCuong:Implement LoginPage

--- lib/screens/login/widgets/body.dart
+++ lib/screens/login/widgets/body.dart
@@ -1,16 +1,35 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:timxe/screens/login/widgets/google_login_button.dart';
+import 'package:timxe/screens/login/widgets/phone_login_button.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      width: double.infinity,
       child: Column(
         children: [
-          SizedBox(
-            height: 100,
-          ),
-          
-          ],
+          Spacer(),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Column(
+                children: [
+                  PhoneButton(),
+                  Row(
+                    children: [
+                      Expanded(child: Divider(height: 40,color: Colors.white,),),
+                      Text("OR",style:TextStyle(color: Colors.white)),
+                      Expanded(child: Divider(height: 40,color: Colors.white,)),
+                    ],
+                  ),
+                  GoogleButtonWidget(),
+                  SizedBox(height: 20,)
+                ],
+              ))
+        ],
       ),
     );
   }
