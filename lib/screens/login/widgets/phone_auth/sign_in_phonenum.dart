fa8e15980c66cdcfb33ffbb8400cf488d2cdae28
tricuongrc86 <cuongnht86.work@gmail.com>
2021-09-28 22:32:07 +0700

    Update src code and launch icon

--- lib/screens/login/widgets/phone_auth/sign_in_phonenum.dart
+++ lib/screens/login/widgets/phone_auth/sign_in_phonenum.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timxe/common/rounded_button.dart';
 import 'package:timxe/screens/login/widgets/phone_auth/otp_screen.dart';
 
 class LoginByPhone extends StatefulWidget {
@@ -11,50 +12,72 @@ class _LoginByPhoneState extends State<LoginByPhone> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // drawer: BackButtonIcon(),
+      // backgroundColor: Colors.greenAccent[400],
       appBar: AppBar(
-        title: Text('Phone Auth'),
+        actions: [
+          Container(
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.arrow_back_ios_rounded),
+            ),
+          )
+        ],
+        title: Text('Đăng nhập SĐT'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Column(children: [
-            Container(
-              margin: EdgeInsets.only(top: 60),
-              child: Center(
-                child: Text(
-                  'Phone Authentication',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 40, right: 10, left: 10),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Phone Number',
-                  prefix: Padding(
-                    padding: EdgeInsets.all(4),
-                    child: Text('+84'),
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                child: Center(
+                  child: Text(
+                    "Nhập Số Điện Thoại",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28,
+                    ),
                   ),
                 ),
-                maxLength: 10,
-                keyboardType: TextInputType.number,
-                controller: _controller,
               ),
-            )
-          ]),
+              Container(
+                margin: EdgeInsets.only(top: 30),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Số điện thoại',
+                    prefix: Padding(
+                      padding: EdgeInsets.all(4),
+                      child: Text('+84'),
+                    ),
+                  ),
+                  maxLength: 10,
+                  keyboardType: TextInputType.number,
+                  controller: _controller,
+                ),
+              )
+            ],
+          ),
           Container(
             margin: EdgeInsets.all(10),
             width: double.infinity,
-            child: FlatButton(
-              color: Colors.blue,
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => OTPScreen(_controller.text)));
-              },
-              child: Text(
-                'Next',
-                style: TextStyle(color: Colors.white),
+            // child: RoundedButton(
+            //   press: () {
+            //     Navigator.of(context).push(MaterialPageRoute(
+            //         builder: (context) => OTPScreen(_controller.text)));
+            //   },
+            //   text: 'Đăng nhập',
+            // ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(17),
+              child: FlatButton(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                color: Colors.greenAccent[400],
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => OTPScreen(_controller.text)));
+                },
+                child: Text('Đăng nhập'),
               ),
             ),
           )
