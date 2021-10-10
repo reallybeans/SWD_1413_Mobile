56e54de03c1104b000ebad2cd7e2e2cfbe9dd196
tricuongrc86 <cuongnht86.work@gmail.com>
2021-10-11 02:09:51 +0700

    Push

--- lib/screens/login/services/get_schedule_api.dart
+++ lib/screens/login/services/get_schedule_api.dart
@@ -16,7 +16,7 @@ class GetScheduleApi {
         "id": 1,
         "nameCustomer": "Richard Nguyen",
         "phone": "0794219089",
-        "start": "12-12-2021",
+        "start": "12-11-2021",
         "timeWait": 3,
         "mode": true,
         "price": 2000000,
@@ -42,13 +42,39 @@ class GetScheduleApi {
         "id": 2,
         "nameCustomer": "Duy Nguyen",
         "phone": "0794219089",
-        "start": "13-12-2021",
+        "start": "23-11-2021",
         "timeWait": 3,
-        "mode": true,
+        "mode": false,
         "price": 2000000,
         "status": 2,
         "schedule": {
-          "total": 4,
+          "total": 2,
+          "address": {
+            "origin": "157 đường 265, quận 9, HCM",
+            "destination": "64 Lũy Bán Bích, Tân Bình, HCM",
+            "waypoint": [
+              "Đường DT,Cần Thơ,Thành Phố Cần Thơ",
+              "Đường MD, An Giang, Tỉnh An Giang"
+            ]
+          },
+          "latlng": {
+            "origin": "10.8449184,106.7905202",
+            "destination": "10.8449184,106.7905202",
+            "waypoint": ["10.8449184,106.7905202", "10.8449184,106.7905202"]
+          }
+        }
+      },
+      {
+        "id": 3,
+        "nameCustomer": "Anh Lê",
+        "phone": "0794219089",
+        "start": "4-12-2021",
+        "timeWait": 3,
+        "mode": false,
+        "price": 2000000,
+        "status": 2,
+        "schedule": {
+          "total": 2,
           "address": {
             "origin": "157 đường 265, quận 9, HCM",
             "destination": "64 Lũy Bán Bích, Tân Bình, HCM",
@@ -65,13 +91,11 @@ class GetScheduleApi {
         }
       }
     ];
+    
     final schedules = list.cast<Map<String, dynamic>>();
     final listofschedules = await schedules.map<Schedule>((json) {
       return Schedule.fromJson(json);
     }).toList();
     return listofschedules;
-    // } else {
-    // throw Exception('');
-    // }
   }
 }
